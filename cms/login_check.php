85099496c59e27472cc6d7bf7ead1b6de489394a
fukiko <kinako.fukiko.0822@gmail.com>
2021-10-13 08:47:35 +0900

    add comment to login_check.php

--- cms/login_check.php
+++ cms/login_check.php
@@ -1,6 +1,7 @@
 <?php
 try {
 
+    // 外部ファイル読み込み
     require_once("../common/common.php");
     require_once("../common/local_settings.php");
 
@@ -9,6 +10,7 @@ try {
     $pass = $post["password"];
     $pass2 = $post["password2"];
 
+    // 入力漏れがある場合
     if (empty($name) === true or empty($pass) === true or empty($pass2) === true) {
         print "ログイン情報が間違っています。<br>" ;
         print "<form>" ;
@@ -17,12 +19,14 @@ try {
         exit();
     }
 
+    // DB接続
     $dsn = "mysql:host=localhost;dbname=blog;charset=utf8";
     $user = "root";
     $password = $db_password;
     $dbh = new PDO($dsn, $user, $password);
     $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    // 入力されたユーザ名・パスワードを持つeditorレコードを抽出
     $sql = "SELECT name FROM editor WHERE name=? && password=?";
     $stmt = $dbh -> prepare($sql);
     $data[] = $name;
@@ -30,6 +34,7 @@ try {
     $stmt -> execute($data);
 
     $rec = $stmt -> fetch(PDO::FETCH_ASSOC);
+    // 合致するeditorレコードがなかった場合
     if (empty($rec["name"]) === true) {
         print "ログイン情報が間違っています。管理者名かパスワードが違います。<br>";
         print "<form>";
@@ -37,6 +42,7 @@ try {
         print "</form>";
         exit();
     }
+    // 合致するeditorレコードがあった場合はセッション開始
     session_start();
     $_SESSION["login"] = 1;
     $_SESSION["name"] = $rec["name"];
@@ -45,7 +51,7 @@ try {
 }
 catch (Exception $e) {
     print "ただ今障害が発生しております。<br>";
-    print $e -> getMessage();
+    // print $e -> getMessage();
     print "<a href='login.php'>戻る</a>";
 }
 ?>
